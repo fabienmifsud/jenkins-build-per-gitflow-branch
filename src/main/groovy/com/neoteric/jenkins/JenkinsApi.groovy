323a943302b39e28b7f37c7a33bb8a305b9d2cb6
mustex <jrbruhnke@gmail.com>
2017-05-16 19:02:40 -0500

    .

--- src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
@@ -92,7 +92,7 @@ class JenkinsApi {
         def newConfig = entryConfig;
         println "START---->$newConfig"
         for(placeholder in placeholders) {
-            def key = "[&$placeholder.key]";
+            def key = "[&amp;$placeholder.key]";
             println "Processing $placeholder.key: $key=>$placeholder.value"
             newConfig = newConfig.replace(key, placeholder.value);
         }
