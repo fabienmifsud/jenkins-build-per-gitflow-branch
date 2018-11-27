00eb8fde7260140a267382f2e51a63e7d18d1bb3
fmifsud <f.mifsud@traxens.com>
2018-11-27 10:12:58 +0100

    Add a temporisation before starting job and add cassandra test option

--- src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsApi.groovy
@@ -120,7 +120,8 @@ class JenkinsApi {
         }
 
         // Add Cassandra test
-        root."mavenOpts" += " -Dtest.cassandra.keyspace=${branchName.replaceAll("-", "_")}"
+        println root.children()
+        root.mavenOpts.value += " -Dtest.cassandra.keyspace=${branchName.replaceAll("-", "_")}"
 
         //check if it was the only parameter - if so, remove the enclosing tag, so the project won't be seen as build with parameters
         def propertiesNode = root.properties
